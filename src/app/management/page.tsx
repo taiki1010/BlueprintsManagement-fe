1dd08b033033273511e6c9608adac0cfcf779e64
taiki1010 <tik86_trueno@yahoo.co.jp>
2025-06-17 13:09:37 +0900

    コンポーネントの表示される文字の修正

--- src/app/management/page.tsx
+++ src/app/management/page.tsx
@@ -7,7 +7,7 @@ const ManagementPage = async() => {
   return (
    <div className="px-10 py-10">
     <p className="text-3xl font-bold">
-      {response.status == 200 ? "図面を選択してください" : "図面を追加してください"}
+      {response.status == 200 ? "現場を選択してください" : "現場を追加してください"}
     </p>
    </div>
   );
