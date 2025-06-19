ad70e8c1dd3b4a24781c5098185d81c600e410e9
taiki1010 <tik86_trueno@yahoo.co.jp>
2025-06-19 17:11:10 +0900

    トップページのデザインを変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const Main = () => {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
-      <div className="text-center">
-        <h2 className="text-3xl mb-4">図面管理システム</h2>
-        <Link href="/management" className="text-2xl text-blue-600 font-bold">管理画面に移動</Link>
+    <div className="relative">
+      <Image src="/images/bg-top.jpg" fill alt=""></Image>
+      <div className="absolute inset-0 bg-orange-100/40"></div>
+      <div className="h-screen w-screen flex justify-center items-center relative">
+        <div className="text-center bg-black/70 px-20 py-20 text-gray-200">
+          <p className="pb-4">大切な図面の管理・共有をシンプルに</p>
+          <h2 className="text-5xl mb-12">図面管理システム</h2>
+          <Link href="/management" className="text-2xl text-blue-600 font-bold bg-white px-4 py-2 rounded-sm cursor-pointer hover:bg-stone-400">管理画面に移動</Link>
+        </div>
       </div>
     </div>
-
   )
 }
 
