3d15448d5d9cfc6bbf49f4eac579198c5f483764
taiki1010 <tik86_trueno@yahoo.co.jp>
2025-06-23 12:05:45 +0900

    alertをmodalに変更

--- src/components/Management/Site/ShareButton/ShareButton.tsx
+++ src/components/Management/Site/ShareButton/ShareButton.tsx
@@ -1,24 +1,27 @@
 "use client";
 
-interface Props {
+import { useState } from "react";
+import ShareModal from "./ShareModal";
+
+interface PropsType {
   id: string;
   name: string;
 }
 
-const ShareButton = ({id, name}: Props) => {
+const ShareButton = ({id, name}: PropsType) => {
 
-  const copyToClipboard = async() => {
-    const url = `${process.env.NEXT_PUBLIC_DOMAIN}/read/sites/${id}`;
-    try {
-      await navigator.clipboard.writeText(url);
-      alert(name + "の共有用URLがクリップボードにコピーされました");
-    } catch(err) {
-      console.error(err);
-      alert("クリップボードへのコピーが失敗しました");
-    }
-  }
+  const [isOpenShareModal, setIsOpenShareModal] = useState<boolean>(false);
+
   return (
-    <button onClick={copyToClipboard} className="text-2xl text-white px-4 rounded-md cursor-pointer bg-blue-500 hover:bg-blue-600">共有</button>
+    <>
+      <div>
+        <button onClick={()=>setIsOpenShareModal(true)} className="text-2xl text-white px-4 h-full rounded-md cursor-pointer bg-blue-500 hover:bg-blue-600">
+          共有
+        </button>
+      </div>
+
+      {isOpenShareModal && <ShareModal id={id} name={name} isOpenShareModal={isOpenShareModal} setIsOpenShareModal={setIsOpenShareModal}/>}
+    </>
   )
 }
 
